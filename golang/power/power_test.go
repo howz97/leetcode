7da59f3cda7ddd3878b2941107a6d23121782d59
zhanghao <zhanghao997@qq.com>
2024-06-08 12:18:28 +0800

    First commit

---
+++ golang/power/power_test.go
@@ -0,0 +1,10 @@
+package power
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPower(t *testing.T) {
+	fmt.Println(Power(9, -10))
+}
---
+++ rust/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod median;
+pub mod fibonacci;
+pub mod pig_latin;
+pub mod substring;
+pub mod palindrome;
+pub mod reverse_k_group;
+pub mod atoi;
+pub mod max_area;
+pub mod util;
